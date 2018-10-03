297f676e75e27626dd2b32890075710a8b373090
Roman Horobets <roman.horobets@rolique.io>
2018-10-03 17:27:55 +0300

    navbarlist

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './styles.css';
+import { FilelistNavbar } from '../FilelistNavbar';
 
 export class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        navbar
+        <FilelistNavbar />
       </div>
     );
   }
