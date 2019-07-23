e2aa18d8d70777a8ba88ffba838c7c8683c81e8d
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-07-24 01:14:45 +0300

    Add MainView

--- fileexplorer/src/App.js
+++ fileexplorer/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+
 import Layout from './components/Layout/Layout';
+import MainView from './containers/MainView/MainView';
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <MainView />
       </Layout>
     </div>
   );
---
+++ fileexplorer/src/containers/MainView/MainView.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Aux';
+
+class MainView extends Component {
+    render () {
+        return (
+            <Aux>
+                <div>Folders ...</div>
+                <div>Files ...</div>
+            </Aux>
+        );
+    }
+}
+
+export default MainView;
