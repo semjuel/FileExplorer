284dacaa99288fc981dcba1ec70cdd23e215c651
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-07-24 20:13:07 +0300

    Change mainview

--- fileexplorer/src/containers/MainView/MainView.js
+++ fileexplorer/src/containers/MainView/MainView.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
 
 import Aux from '../../hoc/Aux';
 
-class MainView extends Component {
-    render () {
-        return (
-            <Aux>
-                <div>Folders ...</div>
-                <div>Files ...</div>
-            </Aux>
-        );
-    }
+const useStyles = makeStyles(theme => ({
+    content: {
+        background: '#E8F1F7',
+        flexGrow: 1,
+        padding: theme.spacing(3),
+    },
+}));
+
+export default function MainView() {
+    const classes = useStyles();
+
+    return (
+        <Aux>
+            <Typography>
+                Table with files....
+            </Typography>
+        </Aux>
+    );
 }
 
-export default MainView;
