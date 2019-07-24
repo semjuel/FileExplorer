47029e3f778c8e4fed1d6fee74ea778aa9b6fcbc
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-07-24 20:12:39 +0300

    Change layout

--- fileexplorer/src/components/Layout/Layout.js
+++ fileexplorer/src/components/Layout/Layout.js
@@ -2,14 +2,70 @@ import React from 'react';
 
 import Aux from '../../hoc/Aux';
 import Header from '../Header/Header';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
 
-const layout = ( props ) => (
-    <Aux>
-        <Header />
-        <main>
-            {props.children}
-        </main>
-    </Aux>
-);
+const drawerWidth = 240;
 
-export default layout;
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        zIndex: 0,
+        position: 'relative',
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        background: '#D3E5EE',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        background: '#E8F1F7',
+    },
+    toolbar: theme.mixins.toolbar,
+}));
+
+export default function Layout( props ) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Header />
+            <Drawer
+                className={classes.drawer}
+                variant="permanent"
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.toolbar} />
+                <List>
+                    {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+            </Drawer>
+            <main className={classes.content}>
+                <div className={classes.toolbar} />
+                {props.children}
+            </main>
+        </div>
+    );
+}
