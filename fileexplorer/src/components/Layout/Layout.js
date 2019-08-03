2569736c4819d900d5db017465df17904f77551a
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-04 00:11:18 +0300

    Create Sidebar component

--- fileexplorer/src/components/Layout/Layout.js
+++ fileexplorer/src/components/Layout/Layout.js
@@ -1,26 +1,9 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
-import IconButton from '@material-ui/core/IconButton';
-import MoreIcon from '@material-ui/icons/MoreVert';
-import FolderIcon from '@material-ui/icons/Folder';
-import FolderOpenIcon from '@material-ui/icons/FolderOpen';
-import Collapse from '@material-ui/core/Collapse';
-import ExpandLess from '@material-ui/icons/ExpandLess';
-import ExpandMore from '@material-ui/icons/ExpandMore';
-import Typography from '@material-ui/core/Typography';
-import Tooltip from '@material-ui/core/Tooltip';
 
 import Aux from '../../hoc/Aux';
 import Header from '../Header/Header';
-import AddFolderBtn from '../Sidebar/AddFolderBtn/AddFolderBtn';
-
-const drawerWidth = 240;
+import Sidebar from "../Sidebar/Sidebar";
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -28,16 +11,6 @@ const useStyles = makeStyles(theme => ({
     },
     appBar: {
         zIndex: theme.zIndex.drawer + 1,
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        zIndex: 0,
-        position: 'relative',
-    },
-    drawerPaper: {
-        width: drawerWidth,
-        background: '#D3E5EE',
     },
     mainWrapper: {
 
@@ -56,113 +29,13 @@ const useStyles = makeStyles(theme => ({
 export default function Layout( props ) {
     const classes = useStyles();
 
-    const [open, setOpen] = React.useState(true);
-
-    function handleClick() {
-        setOpen(!open);
-    }
-
     return (
         <Aux>
             <Header />
 
             <div className={classes.root}>
-                <Drawer
-                    className={classes.drawer}
-                    variant="permanent"
-                    classes={{
-                        paper: classes.drawerPaper,
-                    }}
-                >
-                    <div className={classes.toolbar} />
+                <Sidebar/>
 
-                    <AddFolderBtn />
-
-                    <List
-                        component="nav"
-                        aria-labelledby="nested-list-subheader"
-                        dense={true}
-                    >
-                        <ListItem button>
-                            <ListItemIcon>
-                                <FolderOpenIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={
-                                <Tooltip title="1 Sent mail Sent mail Sent mailSent mail Sent mail">
-                                    <Typography noWrap display={"block"} component="span">
-                                        Sent mail Sent mail Sent mailSent mail Sent mail
-                                    </Typography>
-                                </Tooltip>
-                            } />
-
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <FolderIcon />
-                            </ListItemIcon>
-                            <ListItemText primary={
-                                <Tooltip title="1 Drafts">
-                                    <Typography noWrap display={"block"} component="span">
-                                        Drafts Drafts Drafts Drafts Drafts
-                                    </Typography>
-                                </Tooltip>
-                            } />
-
-                            <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="Delete">
-                                    <MoreIcon />
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                        <ListItem button onClick={handleClick}>
-                            <ListItemIcon>
-                                <FolderIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                            {open ? <ExpandLess /> : <ExpandMore />}
-
-                            <ListItemSecondaryAction>
-                                <IconButton edge="end" aria-label="Delete">
-                                    <MoreIcon />
-                                </IconButton>
-                            </ListItemSecondaryAction>
-                        </ListItem>
-                        <Collapse in={open} timeout="auto" unmountOnExit>
-                            <List dense={true} component="div" disablePadding>
-                                <ListItem button onClick={handleClick} button className={classes.nested}>
-                                    <ListItemIcon>
-                                        <FolderIcon />
-                                    </ListItemIcon>
-                                    <ListItemText primary="Starred" />
-                                    {open ? <ExpandLess /> : <ExpandMore />}
-
-                                    <ListItemSecondaryAction>
-                                        <IconButton edge="end" aria-label="Delete">
-                                            <MoreIcon />
-                                        </IconButton>
-                                    </ListItemSecondaryAction>
-                                </ListItem>
-
-                                <Collapse in={open} timeout="auto" unmountOnExit>
-                                    <List dense={true} component="div" disablePadding>
-                                        <ListItem button className={classes.nested}>
-                                            <ListItemIcon>
-                                                <FolderIcon />
-                                            </ListItemIcon>
-                                            <ListItemText primary="Starred" />
-
-                                            <ListItemSecondaryAction>
-                                                <IconButton edge="end" aria-label="Delete">
-                                                    <MoreIcon />
-                                                </IconButton>
-                                            </ListItemSecondaryAction>
-                                        </ListItem>
-                                    </List>
-                                </Collapse>
-                            </List>
-                        </Collapse>
-                    </List>
-                </Drawer>
                 <main className={classes.content}>
                     <div className={classes.toolbar} />
                     <div className={classes.mainWrapper}>
---
+++ fileexplorer/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,162 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import MoreIcon from '@material-ui/icons/MoreVert';
+import FolderIcon from '@material-ui/icons/Folder';
+import FolderOpenIcon from '@material-ui/icons/FolderOpen';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+
+import AddFolderBtn from './AddFolderBtn/AddFolderBtn';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        zIndex: theme.zIndex.drawer + 1,
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        zIndex: 0,
+        position: 'relative',
+    },
+    drawerPaper: {
+        width: drawerWidth,
+        background: '#D3E5EE',
+    },
+    mainWrapper: {
+
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        background: '#E8F1F7',
+    },
+    toolbar: theme.mixins.toolbar,
+    nested: {
+        paddingLeft: theme.spacing(4),
+    },
+}));
+
+
+export default function Sidebar( props ) {
+    const classes = useStyles();
+
+    const [open, setOpen] = React.useState(true);
+
+    function handleClick() {
+        setOpen(!open);
+    }
+
+    return (
+        <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className={classes.toolbar} />
+
+            <AddFolderBtn />
+
+            <List
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+                dense={true}
+            >
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderOpenIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={
+                        <Tooltip title="1 Sent mail Sent mail Sent mailSent mail Sent mail">
+                            <Typography noWrap display={"block"} component="span">
+                                Sent mail Sent mail Sent mailSent mail Sent mail
+                            </Typography>
+                        </Tooltip>
+                    } />
+
+                </ListItem>
+                <ListItem button>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={
+                        <Tooltip title="1 Drafts">
+                            <Typography noWrap display={"block"} component="span">
+                                Drafts Drafts Drafts Drafts Drafts
+                            </Typography>
+                        </Tooltip>
+                    } />
+
+                    <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="Delete">
+                            <MoreIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+                <ListItem button onClick={handleClick}>
+                    <ListItemIcon>
+                        <FolderIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Inbox" />
+                    {open ? <ExpandLess /> : <ExpandMore />}
+
+                    <ListItemSecondaryAction>
+                        <IconButton edge="end" aria-label="Delete">
+                            <MoreIcon />
+                        </IconButton>
+                    </ListItemSecondaryAction>
+                </ListItem>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List dense={true} component="div" disablePadding>
+                        <ListItem button onClick={handleClick} button className={classes.nested}>
+                            <ListItemIcon>
+                                <FolderIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Starred" />
+                            {open ? <ExpandLess /> : <ExpandMore />}
+
+                            <ListItemSecondaryAction>
+                                <IconButton edge="end" aria-label="Delete">
+                                    <MoreIcon />
+                                </IconButton>
+                            </ListItemSecondaryAction>
+                        </ListItem>
+
+                        <Collapse in={open} timeout="auto" unmountOnExit>
+                            <List dense={true} component="div" disablePadding>
+                                <ListItem button className={classes.nested}>
+                                    <ListItemIcon>
+                                        <FolderIcon />
+                                    </ListItemIcon>
+                                    <ListItemText primary="Starred" />
+
+                                    <ListItemSecondaryAction>
+                                        <IconButton edge="end" aria-label="Delete">
+                                            <MoreIcon />
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                            </List>
+                        </Collapse>
+                    </List>
+                </Collapse>
+            </List>
+        </Drawer>
+    );
+}
