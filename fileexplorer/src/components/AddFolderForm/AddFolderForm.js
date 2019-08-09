791cad77fddc6c4ca01a77a7ae83a176b83e6fcd
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-09 14:31:01 +0300

    Improve AddFolderForm component

--- fileexplorer/src/components/AddFolderForm/AddFolderForm.js
+++ fileexplorer/src/components/AddFolderForm/AddFolderForm.js
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {makeStyles} from "@material-ui/core";
+import React, { Component, useState, Fragment } from 'react';
+import { withStyles } from "@material-ui/core/styles"
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -8,13 +8,16 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
 import Tooltip from "@material-ui/core/Tooltip";
-import { SnackbarProvider, useSnackbar } from 'notistack';
 import axios from 'axios'
 import { Markup } from 'interweave';
+import { bindActionCreators } from 'redux';
 
 import Aux from '../../hoc/Aux';
+import {addFolder, closeSnackbar, enqueueSnackbar} from "../../actions";
+import PropTypes from "prop-types";
+import {connect} from "react-redux";
 
-const useStyles = makeStyles(theme => ({
+const styles = theme => ({
     closeButton: {
         position: 'absolute',
         right: theme.spacing(1),
@@ -38,81 +41,124 @@ const useStyles = makeStyles(theme => ({
             background: '#006AFE',
         },
     },
-}));
+});
 
-export default function AddFolderForm( props ) {
-    const classes = useStyles();
-    const [folderName, setFolderName] = useState();
-
-    const position = {
-        vertical: 'top',
-        horizontal: 'right',
+class AddFolderForm extends Component {
+    state = {
+        folderName: '',
     };
 
-    const { enqueueSnackbar } = useSnackbar();
+    setFolderName = (name) => {
+        this.setState(prevState => ({
+            folderName: name,
+        }));
+    };
 
-    function createFolder() {
+    createFolder() {
         // @TODO send request to the backend.
+        let self = this;
 
-        enqueueSnackbar('Creating folder...', {anchorOrigin: position,});
+        this.props.enqueueSnackbar({
+            message: 'Creating folder...',
+            options: {
+                key: new Date().getTime() + Math.random(),
+                //variant: 'warning',
+                action: key => (
+                    <Button onClick={() => this.props.closeSnackbar(key)}>dissmiss me</Button>
+                ),
+            },
+        });
 
         // Close modal window.
-        props.close();
+        this.props.close();
 
         // @TODO add valid messages.
         axios.post('http://localhost:9195/admin/file-explorer/entry', {
             path: "/",
-            name: folderName,
+            name: self.state.folderName,
 
         })
-        .then(function (response) {
-            console.log(response);
-            enqueueSnackbar('Folder has been created.', {variant: 'success', anchorOrigin: position,});
-        })
-        // @TODO handle this correctly.
-        .catch(function (error) {
-            let msg = 'Can\'t create a folder.';
-            if (error.response.data) {
-                msg = msg + ' ' + error.response.data.message;
-            }
+            .then(function (response) {
+                console.log(response);
+                self.props.enqueueSnackbar({
+                    message: 'Folder has been created.',
+                    options: {
+                        key: new Date().getTime() + Math.random(),
+                        variant: 'success',
+                        action: key => (
+                            <Button onClick={() => self.props.closeSnackbar(key)}>dissmiss me</Button>
+                        ),
+                    },
+                });
+            })
+            // @TODO handle this correctly.
+            .catch(function (error) {
+                let msg = 'Can\'t create a folder.';
+                if (error.response && error.response.data) {
+                    msg = msg + ' ' + error.response.data.message;
+                }
 
-            enqueueSnackbar(<Markup content={msg} />, {variant: 'error', anchorOrigin: position,})
-        });
+                self.props.enqueueSnackbar({
+                    message: msg,
+                    options: {
+                        key: new Date().getTime() + Math.random(),
+                        variant: 'error',
+                        action: key => (
+                            <Button onClick={() => self.props.closeSnackbar(key)}>dissmiss me</Button>
+                        ),
+                    },
+                });
+            });
     }
 
-    return (
-        <Aux>
-            <SnackbarProvider maxSnack={3} />
-            <DialogTitle id="form-dialog-title">
-                New folder
+    render() {
+        const { classes } = this.props;
 
-                <IconButton aria-label="close" className={classes.closeButton} onClick={props.close}>
-                    <Tooltip title="Close">
-                        <CloseIcon />
-                    </Tooltip>
-                </IconButton>
-            </DialogTitle>
+        return (
+            <Fragment>
+                <DialogTitle id="form-dialog-title">
+                    New folder
 
-            <DialogContent className={classes.content}>
-                <TextField
-                    autoFocus
-                    required
-                    id="folder-name"
-                    label="Enter Folder name"
-                    variant="outlined"
-                    onChange={(e) => setFolderName(e.target.value.trim())}
-                    fullWidth
-                />
-            </DialogContent>
+                    <IconButton aria-label="close" className={classes.closeButton} onClick={this.props.close}>
+                        <Tooltip title="Close">
+                            <CloseIcon />
+                        </Tooltip>
+                    </IconButton>
+                </DialogTitle>
 
-            <DialogActions className={classes.btns}>
-                <Button onClick={props.close} color="primary">
-                    Cancel
-                </Button>
-                <Button className={classes.button} variant="contained" onClick={createFolder} color="primary" disabled={!folderName}>
-                    Create
-                </Button>
-            </DialogActions>
-        </Aux>
-    );
+                <DialogContent className={classes.content}>
+                    <TextField
+                        autoFocus
+                        required
+                        id="folder-name"
+                        label="Enter Folder name"
+                        variant="outlined"
+                        onChange={(e) => this.setFolderName(e.target.value.trim())}
+                        fullWidth
+                    />
+                </DialogContent>
+
+                <DialogActions className={classes.btns}>
+                    <Button onClick={this.props.close} color="primary">
+                        Cancel
+                    </Button>
+                    <Button className={classes.button} variant="contained" onClick={() => this.createFolder()} color="primary" disabled={!this.state.folderName}>
+                        Create
+                    </Button>
+                </DialogActions>
+            </Fragment>
+        )
+    }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    enqueueSnackbar,
+    closeSnackbar,
+    addFolder,
+}, dispatch);
+
+AddFolderForm.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default connect(null, mapDispatchToProps)(withStyles(styles)(AddFolderForm));
