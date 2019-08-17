3880a240d48b0c3b2ae2e58884e2909c907d4bdb
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-17 21:34:07 +0300

    Add node reducer

--- fileexplorer/src/reducers/index.js
+++ fileexplorer/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux'
 import foldersTree from './foldersTree'
 import selected from './selected'
 import snackbar from './snackbar'
+import node from './node'
 
 export default combineReducers({
     tree: foldersTree,
     selected: selected,
-    app: snackbar
+    app: snackbar,
+    node: node
 })
