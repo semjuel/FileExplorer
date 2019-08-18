969f8fe97823e71bf5876513075ea75a68da5470
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-18 22:11:53 +0300

    Change selected

--- fileexplorer/src/reducers/selected.js
+++ fileexplorer/src/reducers/selected.js
@@ -1,6 +1,6 @@
 import { SET_SELECTED } from '../actions';
 
-const defaultState = null;
+const defaultState = 0;
 export default (state = defaultState, action) => {
     switch (action.type) {
         case SET_SELECTED:
