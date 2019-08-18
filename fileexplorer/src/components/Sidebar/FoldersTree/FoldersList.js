4c103634f7756c21aff5f20ef6a004834523963d
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-18 18:29:20 +0300

    Change folder name

--- fileexplorer/src/components/Sidebar/FoldersTree/FoldersList.js
+++ fileexplorer/src/components/Sidebar/FoldersTree/FoldersList.js
@@ -7,20 +7,15 @@ import { connect } from "react-redux";
 import Button from '@material-ui/core/Button';
 import List from '@material-ui/core/List';
 import { Markup } from "interweave";
-import CircularProgress from '@material-ui/core/CircularProgress';
 
-import FolderItemWrapper from "./FolderItem";
-import { closeSnackbar, enqueueSnackbar, addFolder, addFolders } from "../../../actions";
+import { closeSnackbar, enqueueSnackbar, addChildren, addFolder } from "../../../actions";
 import {hashFnv32a} from "../../../services/hash";
+import Tree from './Tree';
 
 const styles = theme => ({
     nothing: {
       fontStyle: 'italic',
-    },
-    center: {
-      textAlign: 'center',
-      paddingTop: theme.spacing(4),
-    },
+    }
 });
 
 const itemStyle = {
@@ -29,7 +24,6 @@ const itemStyle = {
 
 class FoldersList extends Component {
     state = {
-        folders: this.props.folders,
         loading: true,
     };
 
@@ -40,15 +34,20 @@ class FoldersList extends Component {
         axios.get('http://localhost:9195/admin/file-explorer/entry?mode=directory&depth=0')
             .then(function (response) {
                 self.setState({loading: false});
-                let all = response.data.data;
-                all.map(function (el) {
-                    el.key = hashFnv32a(el.name) + Math.random();
-                    el.level = 0;
+                let children = response.data.data;
+                let childIds = [];
+                children.map(function (el) {
+                    el.id = hashFnv32a(el.name) + Math.random();
+                    el.level = 1;
+                    el.childIds = [];
+                    self.props.addFolder(el);
+                    childIds.push(el.id);
                 });
-                self.props.addFolders(all);
+                self.props.addChildren(0, childIds);
             })
             // @TODO handle this correctly.
             .catch(function (error) {
+                console.log(error);
                 self.setState({loading: false});
                 let msg = 'Failed fetching data.';
                 if (error.response && error.response.data) {
@@ -69,37 +68,29 @@ class FoldersList extends Component {
     }
 
     render() {
-        const { classes, folders } = this.props;
-        const loading = this.state.loading ? <CircularProgress /> : (<div className={classes.nothing}>No folders</div>);
-
         return (
             <Fragment>
-                { typeof folders !== 'undefined' && folders.length > 0 ? (
-                    <List
-                        component="nav"
-                        aria-labelledby="nested-list-subheader"
-                        dense={true}
-                    >
-
-                        {folders.map(el => (
-                            <FolderItemWrapper styling={itemStyle} element={el} key={el.key} />
-                        ))}
-
-                    </List>) : (<div className={classes.center}>{loading}</div>) }
+                <List
+                    component="nav"
+                    aria-labelledby="nested-list-subheader"
+                    dense={true}
+                >
+                    <Tree id={0} loading={this.state.loading} styling={itemStyle}/>
+                </List>
             </Fragment>
         );
     }
 }
 
 const mapStateToProps = state => {
-    return { folders: state.tree.folders };
+    return { tree: state.tree };
 };
 
 const mapDispatchToProps = dispatch => bindActionCreators({
     enqueueSnackbar,
     closeSnackbar,
+    addChildren,
     addFolder,
-    addFolders,
 }, dispatch);
 
 FoldersList.propTypes = {
