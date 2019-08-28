94c079b4820e35a69d26c3776bf270e66af35953
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-08-28 18:27:14 +0300

    Improve MainView

--- fileexplorer/src/components/MainView/MainView.js
+++ fileexplorer/src/components/MainView/MainView.js
@@ -5,15 +5,48 @@ import TableView from "./TableView/TableView";
 import GridView from "./GridView/GridView";
 
 class MainView extends Component {
+    constructor(props) {
+        super(props);
+
+        this.getFolders = this.getFolders.bind(this);
+        this.renderView = this.renderView.bind(this);
+    }
+
+    getFolders() {
+        // @TODO find a better way.
+        let folders = [];
+        const self = this;
+        const folder = this.props.folders[this.props.selected];
+        if (typeof folder.childIds !== 'undefined' && folder && folder.childIds.length > 0) {
+            folder.childIds.map(function(index){
+                if (self.props.folders[index]) {
+                    folders.push(self.props.folders[index]);
+                }
+            });
+        }
+
+        return folders;
+    }
+
+    renderView(folders) {
+        return (
+            this.props.view ? (
+                    <TableView folders={folders} />
+                ) : (
+                    <div>
+                        <TableView folders={folders} />
+                        {/*<GridView />*/}
+                    </div>
+                )
+        );
+    }
 
     render() {
+        const folders = this.getFolders();
+
         return (
             <React.Fragment>
-                {this.props.view ? (
-                    <TableView />
-                ) : (
-                    <GridView />
-                )}
+                { folders.length > 0 ? (this.renderView(folders)) : ("empty")}
             </React.Fragment>
         );
     }
@@ -22,6 +55,8 @@ class MainView extends Component {
 function mapStateToProps(state, ownProps) {
     return  {
         view: state.view,
+        folders: state.tree,
+        selected: state.selected,
     };
 }
 
