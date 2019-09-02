2ccd65903239524b9416bd30531732792fe36c03
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-09-02 23:18:20 +0300

    Change LeftSidebar container

--- fileexplorer/src/containers/LeftSidebar/LeftSidebar.js
+++ fileexplorer/src/containers/LeftSidebar/LeftSidebar.js
@@ -2,24 +2,12 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 
-import AddFolderBtn from './AddFolderBtn/AddFolderBtn';
-import FoldersList from './FoldersTree/FoldersList';
+import AddFolderBtn from '../../components/LeftSidebar/AddFolderBtn/AddFolderBtn';
+import FoldersList from '../../components/LeftSidebar/FoldersTree/FoldersList';
 
 const drawerWidth = 300;
 
 const useStyles = makeStyles(theme => ({
-    '@global': {
-        '*::-webkit-scrollbar': {
-            width: '0.4em'
-        },
-        '*::-webkit-scrollbar-track': {
-            '-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.00)'
-        },
-        '*::-webkit-scrollbar-thumb': {
-            backgroundColor: 'rgba(0,140,254,.4)',
-            outline: '1px solid slategrey'
-        }
-    },
     drawer: {
         width: drawerWidth,
         flexShrink: 0,
@@ -33,7 +21,7 @@ const useStyles = makeStyles(theme => ({
     toolbar: theme.mixins.toolbar,
 }));
 
-export default function Sidebar( props ) {
+export default function LeftSidebar() {
     const classes = useStyles();
 
     return (
