96b4b305c6a9ef9b4779fc34ac2fa36ca3696f83
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-09-05 01:27:21 +0300

    Remove not needed import

--- fileexplorer/src/components/Modal/Modal.js
+++ fileexplorer/src/components/Modal/Modal.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
-import {withStyles} from "@material-ui/core";
 import {bindActionCreators} from "redux";
 import {
     showModal
