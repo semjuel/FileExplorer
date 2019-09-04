f1412f1f4457c88fea87670a295afab9bb274d32
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-09-05 01:26:21 +0300

    Add enqueueErrorSnackbar action

--- fileexplorer/src/actions/snackbar.js
+++ fileexplorer/src/actions/snackbar.js
@@ -1,3 +1,7 @@
+import {Markup} from "interweave";
+import Button from "@material-ui/core/Button";
+import React from "react";
+
 export const ENQUEUE_SNACKBAR = 'ENQUEUE_SNACKBAR';
 export const CLOSE_SNACKBAR = 'CLOSE_SNACKBAR';
 export const REMOVE_SNACKBAR = 'REMOVE_SNACKBAR';
@@ -10,6 +14,27 @@ export const enqueueSnackbar = notification => {
         notification: {
             ...notification,
             key: key || new Date().getTime() + Math.random(),
+        },
+    };
+};
+
+export const enqueueErrorSnackbar = error => {
+    let msg = 'Failed fetching data.';
+    if (error.response && error.response.data) {
+        msg = msg + ' ' + error.response.data.message;
+    }
+
+    return {
+        type: ENQUEUE_SNACKBAR,
+        notification: {
+            message: <Markup content={msg} />,
+            options: {
+                variant: 'error',
+                action: key => (
+                    <Button onClick={() => closeSnackbar(key)}>dissmiss me</Button>
+                ),
+            },
+            key: new Date().getTime() + Math.random(),
         },
     };
 };
