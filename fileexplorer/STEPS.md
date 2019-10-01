40b4eaadc93784aece532f6c8a3190edcae23744
Igor Semenyuk <igor.semenyuk@rolique.io>
2019-10-02 00:36:13 +0300

    Add GridFileItem

--- fileexplorer/STEPS.md
+++ fileexplorer/STEPS.md
@@ -6,10 +6,10 @@
         - Modal
         - Messages (reuse Modal)
             - Message
-        - Toolbar
+        - Header
             - Logo
             - Search
-            - Actions
+            - ActionsF
                 - Action
             - Settings
         - SideBar
@@ -39,3 +39,21 @@
     
 
 ## Components vs Containers
+
+https://medium.com/material-ui/material-ui-v4-is-out-4b7587d1e701
+https://material-ui.com/api/app-bar/
+
+
+
+https://redux.js.org/basics/example
+https://react-redux.js.org/introduction/quick-start
+https://habr.com/ru/post/269831/
+https://metanit.com/web/react/5.3.php
+
+https://medium.com/hackernoon/redux-patterns-caching-an-api-response-f85f8d8d73c6
+https://github.com/reduxjs/redux/tree/master/examples/tree-view
+https://codesandbox.io/embed/github/reduxjs/redux/tree/master/examples/tree-view
+
+
+https://github.com/danilowoz/react-content-loader
+http://danilowoz.com/create-content-loader/
---
+++ fileexplorer/src/components/MainView/GridView/GridFileItem.js
@@ -0,0 +1,67 @@
+import React, {Component} from "react";
+
+import {noop} from "../../../utils/utils";
+import ClickResolver from "../../../hoc/ClickResolver";
+import Grid from "@material-ui/core/Grid";
+import FileIcon from '@material-ui/icons/FileCopy';
+import Tooltip from "@material-ui/core/Tooltip";
+import Typography from "@material-ui/core/Typography";
+import ButtonBase from "@material-ui/core/ButtonBase";
+
+export class ClickableFileItem extends Component {
+    render() {
+
+        const { index, classes, child, showTooltip } = this.props;
+        console.log('render ClickableFileItem', index);
+        return (
+            <Grid item xs={2} className={classes.grid}>
+                <ButtonBase
+                    onClick={()=>this.props.onClick(index)}
+                    onDoubleClick={()=>this.props.onDoubleClick(index)}
+                    focusRipple
+                    key={child.id}
+                    className={classes.btn}
+                    focusVisibleClassName={classes.focusVisible}
+                >
+                    <span className={classes.backdrop} />
+                    <span className={classes.iconWrap}>
+                        {
+                            child.link ?
+                                <img width={50} height={50} alt={''} src={child.link} /> :
+                                <FileIcon className={classes.folderIcon} />
+                        }
+                    </span>
+                    <span className={classes.name}>
+                        {
+                            showTooltip
+                                ? <Tooltip title={child.name}>
+                                    <Typography noWrap display={"block"} component="span">
+                                        {child.name}
+                                    </Typography>
+                                </Tooltip>
+                                : <Typography noWrap display={"block"} component="span">
+                                    {child.name}
+                                </Typography>
+                        }
+
+                    </span>
+                </ButtonBase>
+            </Grid>
+        );
+    }
+}
+
+ClickableFileItem.defaultProps = {
+    onClick: noop,
+    onDoubleClick: noop,
+};
+
+const EnhancedClickableItem = ClickResolver(ClickableFileItem);
+
+const GridFileItem = (props) => (
+    <EnhancedClickableItem
+        {...props}
+    />
+);
+
+export default GridFileItem;
